76aa4fc0534bb9983eb550ef97cde96a22402e48
wanghuabing <wanghuabing@tal.com>
2021-11-01 15:39:14 +0800

    add responseResult

--- trans-base/src/main/java/com/trans/constants/ApiTypeConstant.java
+++ trans-base/src/main/java/com/trans/constants/ApiTypeConstant.java
@@ -1,9 +1,11 @@
 package com.trans.constants;
 
 public class ApiTypeConstant {
+    public static final String SERVICE_TYPE_AUTH = "auth";
     public static final String SERVICE_TYPE_WEB = "system";
     public static final String SERVICE_TYPE_APP = "app";
     public static final String SERVICE_TYPE_OPEN = "open";
+    public static final String AUTH_BASE_PATH = "/" + SERVICE_TYPE_AUTH + "/api";
     public static final String WEB_BASE_PATH = "/" + SERVICE_TYPE_WEB + "/api";
     public static final String APP_BASE_PATH = "/" + SERVICE_TYPE_APP + "/api";
     public static final String OPEN_BASE_PATH = "/" + SERVICE_TYPE_OPEN + "/api";
