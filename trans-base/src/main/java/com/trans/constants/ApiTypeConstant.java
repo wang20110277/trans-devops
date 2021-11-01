513071bd2f2ac36321efb07c630d5b494c8cb959
wanghuabing <wanghuabing@tal.com>
2021-11-01 10:53:07 +0800

    add responseResult

--- trans-base/src/main/java/com/trans/constants/ApiTypeConstant.java
+++ trans-base/src/main/java/com/trans/constants/ApiTypeConstant.java
@@ -1,10 +1,10 @@
 package com.trans.constants;
 
 public class ApiTypeConstant {
-    public static final String SERVICE_TYPE_OPEN = "open";
     public static final String SERVICE_TYPE_WEB = "system";
     public static final String SERVICE_TYPE_APP = "app";
-    public static final String OPEN_BASE_PATH = "/" + SERVICE_TYPE_OPEN + "/api";
+    public static final String SERVICE_TYPE_OPEN = "open";
     public static final String WEB_BASE_PATH = "/" + SERVICE_TYPE_WEB + "/api";
     public static final String APP_BASE_PATH = "/" + SERVICE_TYPE_APP + "/api";
+    public static final String OPEN_BASE_PATH = "/" + SERVICE_TYPE_OPEN + "/api";
 }
---
+++ trans-base/src/main/java/com/trans/response/ResponseCodeEnum.java
@@ -0,0 +1,39 @@
+package com.trans.response;
+
+public enum ResponseCodeEnum {
+    SUCCESS(100, "请求成功", "请求成功描述");
+
+    private int code;
+    private String msg;
+    private String desc;
+
+    ResponseCodeEnum(int code, String msg, String desc) {
+        this.code = code;
+        this.msg = msg;
+        this.desc = desc;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
